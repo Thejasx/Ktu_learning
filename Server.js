d90d1e5e7796e58d8192d3ec992a250e90c4f699
thejas <thejassathyesh@gmail.com>
2025-12-05 19:21:26 +0530

    middileware done

--- Server.js
+++ Server.js
@@ -8,10 +8,18 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
 app.get('/', (req, res) => {
     console.log(req);
     res.send("hello world");
 });
+
+app.post('/create', (req, res) => {
+    console.log(req.body)
+    res.send("data created")
+})
 
 
 connectdb();
